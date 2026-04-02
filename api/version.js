6b9281f5aed260d77ffac7c9d1d029015f2a8f92
Hamidul Islam <iteducationcenter77@gmail.com>
2026-04-02 12:15:57 +0530

    fix(api): remove require package.json, hardcode version for Vercel static deploy

--- api/version.js
+++ api/version.js
@@ -1,11 +1,10 @@
-const pkg = require('../package.json');
 
 module.exports = async (req, res) => {
   try {
     res.setHeader('Cache-Control', 'no-store');
     res.json({
-      version: pkg.version || '0.0.0',
-      name: pkg.name || 'vectora',
+      version: '1.0.0', // hardcoded version
+      name: 'vectora',
       time: new Date().toISOString(),
     });
   } catch (err) {
